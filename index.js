c2a321e5fc06bc20e8ba79d8c48481f2bb06b8d6
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-30 16:37:58 +0600

    send data id and catagories

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
 const catagoris = require('./data/catagoris.json')
+const news = require('./data/news.json')
+
+
 const cors = require('cors')
 
 app.use(cors())
 
-app.get('/', (req , res)=>{
+app.get('/', (req, res) => {
     res.send('Dragon is running hlw')
 })
 
-app.get('/catagoris', (req , res)=>{
+app.get('/catagoris', (req, res) => {
     res.send(catagoris)
 })
 
-app.listen(port , ()=>{
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/catagoris/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id == 0) {
+        res.send(news)
+    }
+    else {
+        const catagoryNews = news.filter(n => n.category_id === id)
+        res.send(catagoryNews)
+    }
+})
+
+app.listen(port, () => {
     console.log(`Dragon Api is running on ${port}`)
 })
